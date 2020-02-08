b2b8a7e35c71e2a24e8ae4c5e63dfa204b94e517
Victor Wardi <victorcaetano@live.com>
2020-02-08 11:06:31 -0300

    First Commit - Project Configured.

---
+++ src/main/java/com/waes/scalableweb/ScabableWebApplication.java
@@ -0,0 +1,13 @@
+package com.waes.scalableweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScabableWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScabableWebApplication.class, args);
+    }
+
+}
